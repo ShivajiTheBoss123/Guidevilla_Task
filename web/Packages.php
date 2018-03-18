a7a0202e000ddf389f3a9f9756f1e35c1424848f
ShivajiTheBoss123 <35867858+ShivajiTheBoss123@users.noreply.github.com>
2018-03-18 20:34:24 +0530

    php_task
    guidvilla_task

---
+++ web/Packages.php
@@ -0,0 +1,200 @@
+  <!-- Packages chocolat -->
+	<div class="w3ls-galley" id="gallery">
+		<div class="container">
+			<h3 class="center">Packages Offered by vishal</h3>
+			<div class="chocolat-parent" id="parentHorizontalTab" data-chocolat-title="Go-Fly Services">
+				<ul class="resp-tabs-list hor_1">
+							<li><a href="#tab1" class="tabs"><span>All</span></a></li>
+							<li><a href="#tab2" class="tabs"><span>Tourism</span></a></li>
+							<li><a href="#tab3" class="tabs"><span>Honeymoon</span></a></li>
+							<li><a href="#tab4" class="tabs"><span>Pilgrimage</span></a></li>
+				</ul>
+				<div class="resp-tabs-container hor_1">
+					<!-- Tab-1 -->
+					
+					<div>
+						<div class="col-md-3 col-sm-6 col-xs-6 taba">
+							<a class="chocolat-image" href="images/holy.jpg" title="Go-Fly Services">
+								<img class="img-responsive" alt="image" src="images/holy.jpg" />
+							</a>
+							<h4 class="price"> ₹1500 - 2 days 1 night</h4>
+							<p>Holy</p>
+							<a href="package_details.php?dtl=holy" class="register scroll">Request Details</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 taba">
+							<a class="chocolat-image" href="images/taaj.jpg" title="Go-Fly Services">
+								<img class="img-responsive" alt="image" src="images/taaj.jpg" />
+							</a>
+							<h4 class="price"> ₹1500 - 2 days 1 night</h4>
+							<p>Taaj Mahal</p>
+							<a href="package_details.php?dtl=taaj" class="register scroll">Request Details</a>
+						</div>
+						
+						<div class="col-md-3 col-sm-6 col-xs-6 taba">
+							<a class="chocolat-image" href="images/rome1.jpg" title="Go-Fly Services">
+								<img class="img-responsive" alt="image" src="images/rome1.jpg"/>
+							</a>
+							<h4 class="price"> ₹1500 - 2 days 1 night</h4>
+							<p>Rome</p>
+							<a href="package_details.php?dtl=rome1" class="register scroll">Request Details</a>			
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 taba">
+							<a class="chocolat-image" href="images/paris.jpg" title="Go-Fly Services">
+								<img class="img-responsive" alt="image" src="images/paris.jpg"/>
+							</a>
+							<h4 class="price"> ₹1500 - 2 days 1 night</h4>
+							<p>Paris</p>
+							<a href="package_details.php?dtl=paris" class="register scroll">Request Details</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 taba">
+							<a class="chocolat-image" href="images/nigara.jpg" title="Go-Fly Services">
+								<img class="img-responsive" alt="image" src="images/nigara.jpg"/>
+							</a>
+							<h4 class="price"> ₹1500 - 2 days 1 night</h4>
+							<p>Nigara</p>
+							<a href="package_details.php?dtl=nigara" class="register scroll">Request Details</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 taba">
+							<a class="chocolat-image" href="images/jordan.jpg" title="Go-Fly Services">
+								<img class="img-responsive" alt="image" src="images/jordan.jpg"/>
+							</a>
+							<h4 class="price"> ₹1500 - 2 days 1 night</h4>
+							<p>Jordan</p>
+							<a href="package_details.php?dtl=jordan" class="register scroll">Request Details</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 taba">
+							<a class="chocolat-image" href="images/london.jpg" title="Go-Fly Services">
+								<img class="img-responsive" alt="image" src="images/london.jpg"/>
+							</a>
+							<h4 class="price"> ₹1500 - 2 days 1 night</h4>
+							<p>London</p>
+							<a href="package_details.php?dtl=london" class="register scroll">Request Details</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 taba">
+							<a class="chocolat-image" href="images/italy.jpg" title="Go-Fly Services">
+								<img class="img-responsive" alt="image" src="images/italy.jpg"/>
+							</a>
+							<h4 class="price"> ₹1500 - 2 days 1 night</h4>
+							<p>Italy</p>
+							<a href="package_details.php?dtl=italy" class="register scroll">Request Details</a>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+					<!-- //tab-1 -->
+					<!-- tab-2 -->
+					<div>
+					
+						<div class="col-md-3 col-sm-6 col-xs-6 taba">
+							<a class="chocolat-image" href="images/nigara.jpg" title="Go-Fly Services">
+								<img class="img-responsive" alt="image" src="images/nigara.jpg" />
+							</a>
+							<h4 class="price"> ₹1500 - 2 days 1 night</h4>
+							<p>Nigara</p>
+							<a href="package_details.php?dtl=nigara" class="register scroll">Request Details</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 taba">
+							<a class="chocolat-image" href="images/taaj.jpg" title="Go-Fly Services">
+								<img class="img-responsive" alt="image" src="images/taaj.jpg" />
+							</a>
+							<h4 class="price"> ₹1500 - 2 days 1 night</h4>
+							<p>Taaj Mahal</p>
+							<a href="package_details.php?dtl=taaj" class="register scroll">Request Details</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 taba">
+							<a class="chocolat-image" href="images/jordan.jpg" title="Go-Fly Services">
+								<img class="img-responsive" alt="image" src="images/jordan.jpg"/>
+							</a>
+							<h4 class="price"> ₹1500 - 2 days 1 night</h4>
+							<p>Jordan</p>
+							<a href="package_details.php?dtl=jordan" class="register scroll">Request Details</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 taba">
+							<a class="chocolat-image" href="images/italy.jpg" title="Go-Fly Services">
+								<img class="img-responsive" alt="image" src="images/italy.jpg"/>
+							</a> 
+							<h4 class="price"> ₹1500 - 2 days 1 night</h4>
+							<p>Italy</p>
+							<a href="package_details.php?dtl=italy" class="register scroll">Request Details</a>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+					<!-- //tab-2 -->
+					<!-- tab-3 -->
+					<div>
+						<div class="col-md-3 col-sm-6 col-xs-6 taba">
+							<a class="chocolat-image" href="images/paris.jpg" title="Go-Fly Services">
+								<img class="img-responsive" alt="image" src="images/paris.jpg" />
+							</a>
+							<h4 class="price"> ₹1500 - 2 days 1 night</h4>
+							<p>Paris</p>
+							<a href="package_details.php?dtl=paris" class="register scroll">Request Details</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 taba">
+							<a class="chocolat-image" href="images/london.jpg" title="Go-Fly Services">
+								<img class="img-responsive" alt="image" src="images/london.jpg" />
+							</a>
+							<h4 class="price"> ₹1500 - 2 days 1 night</h4>
+							<p>London</p>
+							<a href="package_details.php?dtl=london" class="register scroll">Request Details</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 taba">
+							<a class="chocolat-image" href="images/rome1.jpg" title="Go-Fly Services">
+								<img class="img-responsive" alt="image" src="images/rome1.jpg"/>
+							</a>
+							<h4 class="price"> ₹1500 - 2 days 1 night</h4>
+							<p>Rome</p>
+							<a href="package_details.php?dtl=rome1" class="register scroll">Request Details</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 taba">
+							<a class="chocolat-image" href="images/jordan.jpg" title="Go-Fly Services">
+								<img class="img-responsive" alt="image" src="images/jordan.jpg"/>
+							</a>
+							<h4 class="price"> ₹1500 - 2 days 1 night</h4>
+							<p>Jordan</p>
+							<a href="package_details.php?dtl=jordan" class="register scroll">Request Details</a>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+					<!-- //tab-3 -->
+					<!-- tab-4 -->
+					<div>
+						<div class="col-md-3 col-sm-6 col-xs-6 taba">
+							<a class="chocolat-image" href="images/holy.jpg" title="Go-Fly Services">
+								<img class="img-responsive" alt="image" src="images/holy.jpg" />
+							</a>
+							<h4 class="price"> ₹1500 - 2 days 1 night</h4>
+							<p>Holy</p>
+							<a href="package_details.php?dtl=holy" class="register scroll">Request Details</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 taba">
+							<a class="chocolat-image" href="images/rome1.jpg" title="Go-Fly Services">
+								<img class="img-responsive" alt="image" src="images/rome1.jpg" />
+							</a>
+							<h4 class="price"> ₹1500 - 2 days 1 night</h4>
+							<p>Rome</p>
+							<a href="package_details.php?dtl=rome1" class="register scroll">Request Details</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 taba">
+							<a class="chocolat-image" href="images/taaj.jpg" title="Go-Fly Services">
+								<img class="img-responsive" alt="image" src="images/taaj.jpg"/>
+							</a> 
+							<h4 class="price"> ₹1500 - 2 days 1 night</h4>
+							<p>Taaj Mahal</p>
+							<a href="package_details.php?dtl=taaj" class="register scroll">Request Details</a>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-6 taba">
+							<a class="chocolat-image" href="images/jordan.jpg" title="Go-Fly Services">
+								<img class="img-responsive" alt="image" src="images/jordan.jpg"/>
+							</a> 
+							<h4 class="price"> ₹1500 - 2 days 1 night</h4>
+							<p>Jordan</p>
+							<a href="package_details.php?dtl=jordan" class="register scroll">Request Details</a>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+					<!-- //tab-4 -->
+				</div> <!-- container end-->
+			</div>
+		</div>
+	</div>
+	<!-- //Packages -->
